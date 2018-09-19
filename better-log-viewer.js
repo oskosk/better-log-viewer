a0d08b1050237e34b8d70eca9b32ffef8f00ff67
oskosk <oskosk@gmail.com>
2018-09-19 03:14:19 +0000

    Add proper tailing of file

--- better-log-viewer.js
+++ better-log-viewer.js
@@ -1,9 +1,9 @@
 (function( el, render ) {
 	document.addEventListener('DOMContentLoaded', main, false);
 	function main() {
-		var container = document.querySelector('#better-log-viewer-container');
+		var container = document.querySelector('#better-log-viewer-scroll');
 		if ( ! container ) {
-			console.info( 'Could not find element #better-log-viewer-container' );
+			console.info( 'Could not find element #better-log-viewer-scroll' );
 			return;
 		}
 		render( el( LogViewer ) , container);
@@ -17,11 +17,25 @@
 			};
 		}
 		componentDidMount() {
-			wp.apiFetch( { path: '/better-log-viewer/v1/debug.log' } )
-				.then( data => { this.setState( { logLines: data } ) } );
+			const timeout = () => {
+				setTimeout( () => {
+					wp.apiFetch( { path: '/better-log-viewer/v1/debug.log' } )
+						.then( data => { this.setState( { logLines: data } ) } );
+					timeout();
+				}, 1000);
+			}
+			timeout();
 		}
 		render() {
-			return el( 'div', {}, 
+			return el( 'div',
+				{
+					style: {
+						height: '500px',
+						width: '100%',
+						overflow: 'auto',
+						fontFamily: 'courier',
+					},
+				}, 
 				this.state.logLines.map( line => {
 					return el( Line, {}, line );
 				} )
--- better-log-viewer.php
+++ better-log-viewer.php
@@ -74,7 +74,7 @@ function register_endpoints_blv() {
 
 function show_debug_log_blv() {
 	$debuglog = wpdebuglog_path_blv();
-	$contents = file_get_contents( $debuglog );	
+	$contents = tailCustom_blv( $debuglog, 100 );	
 	return array_reverse( explode( "\n", $contents ) );
 }
 
@@ -87,7 +87,15 @@ function add_menu_page_blv() {
 		'Better Log Viewer',
 		'manage_options',
 		'better-log-viewer',
-		function () { echo '<div id="better-log-viewer-container"></div>'; }
+		function () { 
+			?>
+			<div id="better-log-viewer-container">
+				<h1>Better Log Viewer</h1>
+				<div id="better-log-viewer-scroll">
+				</div>
+			</div>
+			<?php
+		}
 	);
 	add_action( 'admin_head', 'hide_update_notices_blv', 1 );
 
@@ -97,4 +105,51 @@ function hide_update_notices_blv() {
 	if ( page_is_better_log_viewer_blv() ) {
 		remove_all_actions( 'admin_notices' );
 	}
+}
+
+/**
+ * Slightly modified version of http://www.geekality.net/2011/05/28/php-tail-tackling-large-files/
+ * @author Torleif Berger, Lorenzo Stanco
+ * @link http://stackoverflow.com/a/15025877/995958
+ * @license http://creativecommons.org/licenses/by/3.0/
+ */
+function tailCustom_blv($filepath, $lines = 1, $adaptive = true) {
+	// Open file
+	$f = @fopen($filepath, "rb");
+	if ($f === false) return false;
+	// Sets buffer size, according to the number of lines to retrieve.
+	// This gives a performance boost when reading a few lines from the file.
+	if (!$adaptive) $buffer = 4096;
+	else $buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
+	// Jump to last character
+	fseek($f, -1, SEEK_END);
+	// Read it and adjust line number if necessary
+	// (Otherwise the result would be wrong if file doesn't end with a blank line)
+	if (fread($f, 1) != "\n") $lines -= 1;
+
+	// Start reading
+	$output = '';
+	$chunk = '';
+	// While we would like more
+	while (ftell($f) > 0 && $lines >= 0) {
+		// Figure out how far back we should jump
+		$seek = min(ftell($f), $buffer);
+		// Do the jump (backwards, relative to where we are)
+		fseek($f, -$seek, SEEK_CUR);
+		// Read a chunk and prepend it to our output
+		$output = ($chunk = fread($f, $seek)) . $output;
+		// Jump back to where we started reading
+		fseek($f, -mb_strlen($chunk, '8bit'), SEEK_CUR);
+		// Decrease our line counter
+		$lines -= substr_count($chunk, "\n");
+	}
+	// While we have too many lines
+	// (Because of buffer size we might have read too many)
+	while ($lines++ < 0) {
+		// Find first newline and remove all text before that
+		$output = substr($output, strpos($output, "\n") + 1);
+	}
+	// Close file and return
+	fclose($f);
+	return trim($output);
 }
